946d430d0dc28565840fa985645e3272c27eb775
AlisonV2 <alison.vandromme007@gmail.com>
2021-08-10 10:44:08 +0200

    added images and navbar component

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'bootstrap';
+import '@/assets/styles/app/app.scss';
+import AppButton from '@/components/App/Button.vue'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+app.component('AppButton', AppButton);
+
+app.use(store);
+app.use(router);
+app.mount('#app');
